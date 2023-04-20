385c77343f73201edb48a514ccbf429dfa2aad41
Darren Abbey <darrenabbey.1@gmail.com>
2023-04-20 15:58:53 -0500

    Update genome install file checking to be aligned with project file checking.

--- scripts_genomes/process_input_files.genome.php
+++ scripts_genomes/process_input_files.genome.php
@@ -4,195 +4,259 @@ fwrite($logOutput, "Before archive decompression.\n");
 fwrite($logOutput, "\t| ext         = ".$ext."\n");
 fwrite($logOutput, "\t| name        = ".$name."\n");
 fwrite($logOutput, "\t| genomePath  = ".$genomePath."\n");
+
 // Deal with compressed archives.
+$currentDir = getcwd(); // get script's path.
 if (strcmp($ext,"zip") == 0) {
 	fwrite($condensedLogOutput, "Decompressing ZIP file : ".$name."\n");
-	fwrite($logOutput, "\t| This is a ZIP archive of : ");
-	$currentDir = getcwd();                             // get script's path.
-	// figure out filename contained in zip archive.
+	fwrite($logOutput, "\t\t| This is a ZIP archive of : \n");
+
+	// figure out first/only filename contained in zip archive.
 	$null               = shell_exec("unzip -l ".$genomePath.$name." > ".$genomePath."zipTemp.txt");   // generate txt file containing archive contents.
 	$zipTempLines       = file($genomePath."zipTemp.txt");
 	$zipTempArchiveLine = trim($zipTempLines[3]);
 	$columns            = preg_split('/\s+/', $zipTempArchiveLine);
 	$oldName            = $columns[3];
-	fwrite($logOutput, " '$oldName'.\n");
 	$fileName_parts     = preg_split('/[.]/', $oldName);
-	fwrite($logOutput,"\t| number of fileName parts = ".count($fileName_parts)."\n");
-	// extract archive.
-	chdir($genomePath);                   // move to genomeDirectory.
+	fwrite($logOutput,"\t\t|\t'".$oldName."'.\n");
+	fwrite($logOutput,"\t\t| The number of fileName parts = ".count($fileName_parts)."\n");
+
+	// What is the file count in the archive?
+	$fileCount          = shell_exec("zipinfo ".$genomePath.$name." |grep Zip|grep -oE '[^ ]+$'");
+	$fileCount          = trim($fileCount);
+	fwrite($logOutput,"\t\t| Files in zip archive = ".$fileCount."\n");
+
+	// Extract archive.
+	chdir($genomePath);                    // move to genome directory.
 	$null = shell_exec("unzip -j ".$name); // unzip archive.
 	chdir($currentDir);                    // move back to script's path.
-	// delete original archive.
+
+	// If more than one file in archive, delete all but first.
+	for ($i = 4; $i < $fileCount+2; $i++) {
+		$zipTempArchiveLine = trim($zipTempLines[$i]);
+		$columns            = preg_split('/\s+/', $zipTempArchiveLine);
+		$fileName           = $columns[3];
+		unlink($genomePath.$fileName);
+	}
+
+	// Delete original archive.
 	unlink($genomePath.$name);
-	// figure out file type.
-	if (count($fileName_parts) == 2) {
-		$ext_new        = end($fileName_parts);
-	} else {
-		// A file extension is not found.
-		// determine if it is a FASTA file by looking at first four lines of text.
-		$currentDir = getcwd();
-		//fwrite($logOutput, "Current directory = '$currentDir'.\n");
-		$file_name = $currentDir."/".$genomePath.$oldName;
-		//fwrite($logOutput, "Open file '".$file_name."'.\n");
-		$file_handle = fopen($file_name,'r');
-		//fwrite($logOutput, "file_handle = '".$file_handle."'\n");
-		$line_1      = fgets($file_handle);
-		$line_2      = fgets($file_handle);
-		$line_3      = fgets($file_handle);
-		$line_4      = fgets($file_handle);
-		fclose($file_handle);
-		if ($line_1[0] == '>') {
-			// is a FASTA file.  This is a very poor criteria, but will fail many other formats.
-			$ext_new = "fasta";
-		} else {
-			$ext_new = "none1";
+
+	// Sanitize name of first file from archive: Replace all "." in $name with "-" except the final one.
+	// Will convert a file with no type to one with an incompatible type. ex "name" to "name.name".
+	$fragments  = explode(".",$oldName);
+	$count      = sizeof($fragments);
+	$name_first = $fragments[0];
+	if ($count > 2) {
+		for ($i = 1; $i < $count-1; $i++) {
+			$name_first .= "-".$fragments[$i];
 		}
 	}
-	$name_new           = $oldName;
+	$name_first .= ".".$fragments[$count-1];
+	$ext_first   = $fragments[$count-1];
+	rename($genomePath.$oldName,$genomePath.$name_first);
+
 	// rename decompressed file.
-	$rename      = $output_fileName;  //"datafile_".$key.".".$ext_new;
-	chdir($genomePath);
-	rename($name_new,$rename);
-	chdir($currentDir);
-	$name_new    = $rename;
+	if ($ext_first == "fa") {
+		// if short extension for fasta, fq is found, rename to fasta.
+		$rename_target = "datafile_".$key.".fasta";
+	} else {
+		$rename_target = "datafile_".$key.".".$ext_first;
+	}
+	rename($genomePath.$name_first,$genomePath.$rename_target);
+
+	fwrite($logOutput, "\t\t| currentDir    = '".$currentDir."'\n");
+	fwrite($logOutput, "\t\t| genomePath    = '".$genomePath."'\n");
+	fwrite($logOutput, "\t\t| oldName       = '".$oldName."'\n");
+	fwrite($logOutput, "\t\t| name_first    = '".$name_first."'\n");
+	fwrite($logOutput, "\t\t| rename_target = '".$rename_target."'\n");
+
+	// Hand off decompressed ZIP file to next section.
+	$ext_new  = $ext_first;
+	$name_new = $rename_target;
+
+	// Is decompressed file a FASTA?
+	if ($ext_new == "fasta") {
+		// FASTA file was found.
+	} else {
+		// FASTA file was not found.
+		unlink($genomePath.$name_first);
+		fwrite($logOutput, "\t\t| FASTA file not found in ZIP!!!\n");
+		$ext_new  = "none1";
+		$name_new = "";
+	}
 } else if (strcmp($ext,"gz") == 0) {
-	fwrite($condensedLogOutput, "Decompressing GZ file : ".$name_new."\n");
-	fwrite($logOutput, "\t| This is a GZ archive of : ");
-	// figure out filename contained in gz archive.
-	$null               = shell_exec("gzip -l ".$genomePath.$name." > ".$genomePath."gzTemp.txt");   // generate txt file containing archive contents.
-	$gzTempLines        = file($genomePath."gzTemp.txt");
-	$gzTempArchiveLine  = trim($gzTempLines[1]);
-	$columns            = preg_split('/\s+/', $gzTempArchiveLine);
-	$oldName            = $columns[3];
-	$fileName_parts     = preg_split('/[.]/', $oldName);
-	fwrite($logOutput, " '$oldName'.\n");
-	fwrite($logOutput,"\t| number of fileName parts = ".count($fileName_parts)."\n");
-	// extract archive.
-	$currentDir = getcwd();                // get script's path.
-	chdir($genomePath);                    // move to genomeDirectory.
-	$null = shell_exec("gzip -d ".$name);  // decompress archive.
-	chdir($currentDir);                    // move back to script's path.
-	// Figure out file type.
-	if (count($fileName_parts) == 4) {
-		// A file extension is found.
-		// For example:
-		//    [0] = '.';
-		//    [1] = '.';
-		//    [2] = '/users/darren1/genomes/test_gz4/test';
-		//    [3] = 'fastq';
-		$fileName_parts2 = preg_split('/[\/]/', $fileName_parts[count($fileName_parts)-2] );
-		$oldName         = end($fileName_parts2).".".end($fileName_parts);
-		$ext_new         = end($fileName_parts);
+	fwrite($condensedLogOutput, "Decompressing GZ file : ".$name."\n");
+	fwrite($logOutput, "\t\t| This is a GZ archive of : ".$name."\n");
+
+	// What is the file count in the archive?
+	$fileCount          = shell_exec("tar -tzf ".$genomePath.$name." | wc -l");
+
+	// Extract archive.
+	if ($fileCount == 0) {
+		// Is not a tar.gz, so decompress with gzip.
+		chdir($genomePath);                   // move to genome directory.
+		$null = shell_exec("gzip -d ".$name);  // decompress archive.
+		chdir($currentDir);                    // move back to script's path.
+
+		// Figure out filename contained in gz archive.
+		// If one file, then filename is same as archive, without gz.
+		$name_new   = str_replace(".gz","", $name);
+		$name_final = str_replace("-fasta",".fasta",$name_new);
+		$name_final = str_replace("-FASTA",".fasta",$name_final);
+		$name_final = str_replace("-fa",".fasta",$name_final);
+		$name_final = str_replace("-FA",".fasta",$name_final);
+
+		$name_first = $name_new;
+		$name_ext   = pathinfo($name_final, PATHINFO_EXTENSION);
 	} else {
-		// A file extension is not found.
-		// For example:
-		//    [0] = '.';
-		//    [1] = '.';
-		//    [2] = '/users/darren1/genomes/test_gz4/test';
-		$fileName_parts2 = preg_split('/[\/]/', end($fileName_parts) );
-		$oldName         = end($fileName_parts2);
-		// determine if it is a FASTQ file by looking at first four lines of text.
-		$currentDir      = getcwd();
-		// fwrite($logOutput, "Current directory = '$currentDir'.\n");
-		$file_name       = $currentDir."/".$genomePath.$oldName;
-		// fwrite($logOutput, "Open file '".$file_name."'.\n");
-		$file_handle     = fopen($file_name,'r');
-		// fwrite($logOutput, "file_handle = '".$file_handle."'\n");
-		$line_1          = fgets($file_handle);
-		$line_2          = fgets($file_handle);
-		$line_3          = fgets($file_handle);
-		$line_4          = fgets($file_handle);
-		fclose($file_handle);
-		if ($line_1[0] == '>') {
-			// is a FASTA file.  This is a very poor criteria, but will fail many other formats.
-			$ext_new = "fasta";
-		} else {
-			$ext_new = "none1";
+		fwrite($logOutput,"\t\t| Files in tar.gz archive = ".$fileCount.".\n");
+
+		// multiple files found in archive, so is a tar.gz and needs different handling.
+		chdir($genomePath);                   // move to genome directory.
+		$file_list = shell_exec("tar xvzf ".$name);
+		chdir($currentDir);                    // move back to script's path.
+
+		// Figure out individual filenames from tar.gz
+		$files = explode("\n",$file_list);
+
+		// If more than one file in archive, delete all but first.
+		$fileCount = size_of($files);
+		if ($fileCount > 1) {
+			for ($i = 1; $i < $fileCount; $i++) {
+				$fileName = $files[$i];
+				unlink($genomePath.$fileName);
+			}
 		}
+
+		// Delete original archive.
+		unlink($genomePath.$name);
+
+		// Sanitize name of first file from archive: Replace all "." in $name with "-" except the final one.
+		// Will convert a file with no type to one with an incompatible type. ex "name" to "name.name".
+		$fragments  = explode(".",$files[0]);
+		$count      = sizeof($fragments);
+		$name_first = $fragments[0];
+		if ($count > 2) {
+			for ($i = 1; $i < $count-1; $i++) {
+				$name_first .= "-".$fragments[$i];
+			}
+		}
+		$name_first .= ".".$fragments[$count-1];
+		$name_ext    = $fragments[$count-1];
+		rename($genomePath.$files[0],$genomePath.$name_first);
 	}
-	fwrite($logOutput, "\t| Original name within archive = '".$oldName."'.\n");
-	$name_new    = $oldName;
+	$oldName = $name_new;
+
 	// rename decompressed file.
-	$rename      = $output_fileName;   // "datafile_".$key.".".$ext_new;
-	chdir($genomePath);
-	rename($name_new,$rename);
-	chdir($currentDir);
-	$name_new    = $rename;
+	$rename_target = "datafile_".$key.".".$name_ext;
+	rename($genomePath.$name_first,$genomePath.$rename_target);
+	fwrite($logOutput, "\t\t| oldName = '".$oldName."'\n");
+	fwrite($logOutput, "\t\t| rename  = '".$rename_target."'\n");
+
+	// Hand off decompressed GZ file to next section.
+	$ext_new  = $name_ext;
+	$name_new = $rename_target;
+
+	// Is decompressed file a FASTA?
+	if ($ext_new == "fasta") {
+		// FASTA file was found.
+	} else {
+		// FASTA file was not found.
+		unlink($genomePath.$name_first);
+		fwrite($logOutput, "\t\t| FASTA file not found in GZ!!!\n");
+		$ext_new  = "none1";
+		$name_new = "";
+	}
+} else if ($ext == "fa") {
+	// if short extension for fasta, fa is found, rename to fasta.
+	$ext_new  = "fasta";
+	$name_new = $name;
 } else {
-	// extension indicates the file is not a compressed archive.
+	// Not a compressed archive, hand off to next section.
 	$ext_new  = $ext;
 	$name_new = $name;
-
-	$rename = $output_fileName;
-	$currentDir = getcwd();
-	chdir($genomePath);
-	rename($name_new,$rename);
-	chdir($currentDir);
-	$name_new = $rename;
 }
-if (strcmp($ext_new,"") == 0) {
-	// A file extension is not found.
-	// determine if it is a FASTQ file by looking at first four lines of text.
-	$currentDir = getcwd();
-	fwrite($logOutput, "Current directory = '$currentDir'.\n");
-	$file_name = $currentDir."/".$genomePath.$name;
-	fwrite($logOutput, "Open file '".$file_name."'.\n");
+
+fwrite($logOutput, "\t\t| After archive decompression.\n");
+fwrite($logOutput, "\t\t|\text_new     = ".$ext_new."\n");
+fwrite($logOutput, "\t\t|\tname_new    = ".$name_new."\n");
+fwrite($logOutput, "\t\t|\tgenomePath  = ".$genomePath."\n");
+
+
+//=======================================
+// Validate FASTA files.
+//---------------------------------------
+if ($ext_new == "fasta") {
+	// Correct filename.
+
+	// initialize validity.
+	$fasta_valid = true;
+
+	// Looking at all lines in file.
+	$file_name   = $genomePath.$name_new;
 	$file_handle = fopen($file_name,'r');
-	fwrite($logOutput, "file_handle = '".$file_handle."'\n");
-	$line_1      = fgets($file_handle);
-	$line_2      = fgets($file_handle);
-	$line_3      = fgets($file_handle);
-	$line_4      = fgets($file_handle);
-	fclose($file_handle);
-	if ($line_1[0] == '>') {
-		// is a FASTA file.  This is a very poor criteria, but will fail many other formats.
-		$ext_new = "fasta";
+	if ($file_handle) {
+		while (($line = fgets($file_handle)) !== false) {
+			// process the line read.
+			if ($line[0] == '<') {
+				// chromosome header line.
+			} else {
+				// check if line is composed only of ATCGN characters.
+				if (preg_match('/[^ATCGN]/', trim($line))) {
+					// line contains characters other than ATCGN: not expected.
+					$fasta_valid = false;
+				}
+			}
+		}
+		fclose($handle);
+	}
+
+	// Is this a fasta file?
+	if ($fasta_valid == true) {
+		// file contents are consistent with FASTA format.
 	} else {
+		/ format is wrong for a FASTA file.
+		unlink($genomePath.$name_first);
+		fwrite($logOutput, "\t\t| FASTA file format incorrect!!!\n");
 		$ext_new = "none2";
 	}
-	$name_new    = $name;
-	// rename uploaded file with no extension.
-	$rename      = $output_fileName;   // "datafile_".$key.".".$ext_new;
-	chdir($genomePath);
-	rename($name_new,$rename);
-	chdir($currentDir);
-	$name_new    = $rename;
 }
-fwrite($logOutput, "After archive decompression.\n");
-fwrite($logOutput, "\t| ext_new     = ".$ext_new."\n");
-fwrite($logOutput, "\t| name_new    = ".$name_new."\n");
-fwrite($logOutput, "\t| genomePath  = ".$genomePath."\n");
 
-// Deal with raw/decompressed files.
-if ((strcmp($ext_new,"fasta") == 0) || (strcmp($ext_new,"fa") == 0)) {
-	fwrite($logOutput, "\t| This is an uncompressed FASTA file, no further pre-processing is needed.\n");
+
+//================================================
+// Final processing of data files, error logging.
+//------------------------------------------------
+if ($ext_new == "fasta") {
+	fwrite($logOutput, "\t\t| This is a FASTA file, no further pre-processing is needed.\n");
 	fwrite($output, $name_new."\n");
-} elseif (strcmp($ext_new,"tab") == 0) {
-	fwrite($logOutput, "\t| This is an uncompressed TAB file, no further pre-processing is needed.\m");
-	fwrite($output, $name_new."\n");
-} elseif (strcmp($ext_new,"none1") == 0) {
-	fwrite($logOutput, "\t| This archive contained a file with no extension and the file type could not be determined.\n");
-	$errorFile = fopen("../users/".$user."/genomes/".$genome."/error.txt", 'w');
-	fwrite($errorFile, "Error : Archive contained a file with no extension and the file type could not be determined.\nUpload FASTA, or ZIP or GZ archives containing a FASTA file.");
+	$paired = 0;
+} elseif ($ext_new == "none1") {
+	fwrite($logOutput, "\t\t| This archive did not contain a FASTA.\n");
+	$errorFile = fopen("users/".$user."/genomes/".$genome."/error.txt", 'w');
+	fwrite($errorFile, "Error : Archive did not contain FASTA file.");
 	fclose($errorFile);
 	chmod($errorFileName,0755);
 	exit;
-} elseif (strcmp($ext_new,"none2") == 0) {
-	fwrite($logOutput, "\t| This file had no extension and the file type could not be determined.\n");
-	$errorFile = fopen("../users/".$user."/genomes/".$genome."/error.txt", 'w');
-	fwrite($errorFile, "Error : File had no extension and the file type could not be determined.\nUpload FASTA, or ZIP or GZ archives containing a FASTA file.");
+} elseif ($ext_new == "none2") {
+	fwrite($logOutput, "\t\t| The FASTA file was not formated properly.\n");
+	$errorFile = fopen("users/".$user."/genomes/".$genome."/error.txt", 'w');
+	fwrite($errorFile, "Error : FASTA file formatting improperly.");
 	fclose($errorFile);
 	chmod($errorFileName,0755);
 	exit;
 } else {
-	fwrite($logOutput, "\t| This is an unknown file type.\n");
-	$errorFile = fopen("../users/".$user."/genomes/".$genome."/error.txt", 'w');
-	fwrite($errorFile, "Error : Unknown file type as input.\nUpload FASTA, or ZIP or GZ archives containing a FASTA file.");
+	fwrite($logOutput, "\t\t| This is an unknown file type.\n");
+	$errorFile = fopen("users/".$user."/genomes/".$genome."/error.txt", 'w');
+	fwrite($errorFile, "Error : Unknown file type as input.\nSee help tab for details of valid file types.");
 	fclose($errorFile);
 	chmod($errorFileName,0755);
 	exit;
 }
 
+fwrite($logOutput, "\t\t*--------------------------------------------------------*\n");
+fwrite($logOutput, "\t\t| 'process_input_files.genome.php' has completed.         |\n");
+fwrite($logOutput, "\t\t*========================================================*\n");
 return $name_new;
 }
 ?>
