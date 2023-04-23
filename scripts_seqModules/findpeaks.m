0ad127863d241be6b5a8ade337f66f3decc2b9f5
Darren Abbey <darrenabbey.1@gmail.com>
2023-04-23 12:01:49 -0500

    Fixing minor matlab code errors.

--- scripts_seqModules/findpeaks.m
+++ scripts_seqModules/findpeaks.m
@@ -9,8 +9,8 @@ function [pks,locs] = findpeaks(X);
 	pks  = [];
 	locs = [];
 	if (M < 4)
-		datamsgid = generatemsgid('emptyDataSet');
-		error(datamsgid,'Data set must contain at least 4 samples.');
+		%datamsgid = generatemsgid('emptyDataSet');
+		%error(datamsgid,'Data set must contain at least 4 samples.');
 	else
 		for idx = 1:M-3
 			if ((X(idx) < X(idx+1)) && (X(idx+1) >= X(idx+2)) && (X(idx+2) > X(idx+3)))
