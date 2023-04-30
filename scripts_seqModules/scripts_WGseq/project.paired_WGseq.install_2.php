8aa759452b9f277e88de6ccb1acf089cf77d8a8e
Darren Abbey <darrenabbey.1@gmail.com>
2023-04-30 03:52:00 -0500

    Cleaning up tabs.

--- scripts_seqModules/scripts_WGseq/project.paired_WGseq.install_2.php
+++ scripts_seqModules/scripts_WGseq/project.paired_WGseq.install_2.php
@@ -67,14 +67,14 @@
 		fwrite($logOutput, "\t\tPath       : '$projectPath'.\n");
 
 		// Generate 'upload_size.txt' file to contain the size of the uploaded file (irrespective of format) for display in "Manage Datasets" tab.
-        $fileNumber     = $key+1;
-        $output2Name    = $projectPath."upload_size_".$fileNumber.".txt";
-        $output2        = fopen($output2Name, 'w');
-        $fileSizeString = filesize($projectPath.$name);
-        fwrite($output2, $fileSizeString);
-        fclose($output2);
-        chmod($output2Name,0755);
-        fwrite($logOutput, "\tGenerated 'upload_size".$fileNumber.".txt' file.\n");
+		$fileNumber     = $key+1;
+		$output2Name    = $projectPath."upload_size_".$fileNumber.".txt";
+		$output2        = fopen($output2Name, 'w');
+		$fileSizeString = filesize($projectPath.$name);
+		fwrite($output2, $fileSizeString);
+		fclose($output2);
+		chmod($output2Name,0755);
+		fwrite($logOutput, "\tGenerated 'upload_size".$fileNumber.".txt' file.\n");
 
 		// Process the uploaded file.
 		$paired = process_input_files($ext,$name,$projectPath,$key,$user,$project,$output, $condensedLogOutput,$logOutput);
