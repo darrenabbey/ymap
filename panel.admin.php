aa5140ae9cc723954bd8d3e7c9ea1c9cafd61511
Darren Abbey <darrenabbey.1@gmail.com>
2023-05-06 03:13:27 -0500

    Add function to lock user accounts from admin tab of user interface.

--- panel.admin.php
+++ panel.admin.php
@@ -48,6 +48,8 @@ Newly registered locked accounts, pending approval.
 			if (file_exists("users/".$user."/locked.txt")) {
 				echo "\t\t\t<input type='button' value='Approve' onclick=\"key = '$key'; $.ajax({url:'admin.approve_server.php',type:'post',data:{key:key},success:function(answer){console.log(answer);}});location.replace('panel.admin.php');\">\n";
 				echo "\t\t\t<input type='button' value='Delete'  onclick=\"key = '$key'; $.ajax({url:'admin.delete_server.php' ,type:'post',data:{key:key},success:function(answer){console.log(answer);}});location.replace('panel.admin.php');\">\n";
+			} else {
+				echo "\t\t\t<input type='button' value='Lock user' onclick=\"key = '$key'; $.ajax({url:'admin.lockUser_server.php',type:'post',data:{key:key},success:function(answer){console.log(answer);}});location.replace('panel.admin.php');\">\n";
 			}
 
 			echo "\t\t</td></tr>\n";
