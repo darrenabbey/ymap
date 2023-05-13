6f23b8453d4426d6cf2db3009dad04ee5531d43a
Darren Abbey <darrenabbey.1@gmail.com>
2023-05-13 01:48:48 -0500

    Add user quota check before file upload is initiated.

--- php/uploader/index.php
+++ php/uploader/index.php
@@ -2,6 +2,7 @@
 	session_start();
 	error_reporting(E_ALL);
 	require_once '../../constants.php';
+	require_once '../../sharedFunctions.php';
 	require_once '../../POST_validation.php';
 
 	// If the user is not logged on, redirect to login page.
@@ -13,7 +14,7 @@
 	// Load user string from session.
 	$user     = $_SESSION['user'];
 
-// Only information needed by this script, sent by "js/ajaxfileupload.js" from form defined in "uploader.1.php".
+// Only information needed by this script sent by "js/ajaxfileupload.js" from form defined in "uploader.1.php".
 // This safe data is used to construct the target file location.
 	// Sanitize input strings.
 	$genome  = sanitize_POST("target_genome");
@@ -40,13 +41,31 @@
 		$target_dir = "../../users/".$user."/projects/".$project."/";
 	}
 
+	//==================================
+	// Check if user has exceeded quota.
+	//----------------------------------
+	// getting the current size of the user folder in Gigabytes
+	$currentSize = getUserUsageSize($user);
+	// getting user quota in Gigabytes
+	$quota_ = getUserQuota($user);
+	if ($quota_ > $quota) {   $quota = $quota_;   }
+	// Setting boolean variable that will indicate whether the user has exceeded it's allocated space, if true the button to add new dataset will not appear
+	$exceededSpace = $quota > $currentSize ? FALSE : TRUE;
+	if ($exceededSpace) {
+		echo "<script type=\"text/javascript\">\nreload_page=function() {\n\tnparent.update_interface();\n}\n";
+		echo "var intervalID = window.setInterval(reload_page, 1000);\n</script>\n";
+	}
+
 
 	//============================================================================
 	// HTML5Uploader ::  Adam Filkor : http://filkor.org
 	// Licensed under the MIT license : http://www.opensource.org/licenses/MIT
-	//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	// non-MySQL version.
 	//----------------------------------------------------------------------------
-	require('UploadHandler.php');
-	$upload_handler = new UploadHandler($target_dir);
+	// Only allow uploading to proceed if user quota is not exceeded.
+	if (!$exceededSpace) {
+		require('UploadHandler.php');
+		$upload_handler = new UploadHandler($target_dir);
+	}
 ?>
