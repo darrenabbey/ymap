aa4b850effaed0a9b4ffd2faa203d804f33994c9
Darren Abbey <darrenabbey.1@gmail.com>
2023-05-13 01:04:38 -0500

    remove user references in javascript.

--- js/project.delete_index.js
+++ js/project.delete_index.js
@@ -1,24 +1,22 @@
-function deleteProjectConfirmation(user,project,key){
-	console.log("$ user    = '"+user+"'");
+function deleteProjectConfirmation(project,key){
 	console.log("$ project = '"+project+"'");
 	console.log("$ key     = '"+key+"'");
 	panel_iframe             = document.getElementById('panel_manageDataset_iframe');
 	dom_object               = panel_iframe.contentDocument.getElementById('p_delete_'+key);
 
-	dom_object.innerHTML     = "<b><font color=\"red\">[Are you sure?]</font><button type='button' onclick='parent.deleteProject_yes(\""+user+"\",\""+project+"\",\""+key+"\")'>Yes, delete.</button>";
-	dom_object.innerHTML    += "<button type='button' onclick='parent.deleteProject_no(\""+user+"\",\""+project+"\",\""+key+"\")'>No, cancel</button></b>";
+	dom_object.innerHTML     = "<b><font color=\"red\">[Are you sure?]</font><button type='button' onclick='parent.deleteProject_yes(\""+project+"\",\""+key+"\")'>Yes, delete.</button>";
+	dom_object.innerHTML    += "<button type='button' onclick='parent.deleteProject_no(\""+project+"\",\""+key+"\")'>No, cancel</button></b>";
 
 	dom_button               = panel_iframe.contentDocument.getElementById('project_delete_'+key);
 	dom_button.style.display = 'none';
 }
 
-function deleteProject_yes(user,project,key){
+function deleteProject_yes(project,key){
 	console.log('deleteProject_yes');
 	$.ajax({
 		url : 'project.delete_server.php',
 		type : 'post',
 		data : {
-			user: user,
 			project: project
 		},
 		success : function(answer){
@@ -32,7 +30,7 @@ function deleteProject_yes(user,project,key){
 	update_projectsShown_after_project_delete(key);
 }
 
-function deleteProject_no(user,project,key){
+function deleteProject_no(project,key){
 	console.log('deleteProject_no');
 	panel_iframe         = document.getElementById('panel_manageDataset_iframe');
 	dom_object           = panel_iframe.contentDocument.getElementById('p_delete_'+key)
