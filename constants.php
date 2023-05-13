d5ce21f07d3e403d91d53ba81ee09e41e7781dd1
Darren Abbey <darrenabbey.1@gmail.com>
2023-05-13 01:13:36 -0500

    remove logging function.

--- constants.php
+++ constants.php
@@ -13,7 +13,7 @@
 	$directory = ".";
 
 	// hardcoded quota (used in case no globalquota.txt in users folder or no quota.txt exits in user folder)
-	$quota = 10;
+	$quota = 0.010;
 
 	// The following constants stem from the fact that Ymap display up to 50 chromosomes and that php supports up to around 1000 variables that can be passed
 	// between forms and in $_SESSION variables which limits the genome form to up to 300 entries
@@ -22,27 +22,4 @@
 
 	// Pepper string for password security.
 	include(".pepper.php");
-
-	// YMAP logging function.
-	function log_stuff($where,$user,$project,$hapmap,$genome,$filename,$message) {
-		// define log file.
-		$log_file = $where."logs/".date('Y-m-d')."_activity.log";
-
-		// check if log file exists, create if not.
-		if (!file_exists($log_file)) {
-			$myfile = fopen($log_file, "w");
-			fwrite($myfile, "Initiate log file: ".date('Y-m-d H:i:s')."\n");
-			fclose($myfile);
-		}
-
-		// add comment to log file.
-		$line = date('Y-m-d H:i:s').' - IP:'.$_SERVER["REMOTE_ADDR"].' - SessionID:'.session_id();
-		if (!empty($user)) {		$line = $line.' - user:'.$user;		}
-		if (!empty($project)) {		$line = $line.' - project:'.$project;	}
-		if (!empty($hapmap)) {		$line = $line.' - hapmap:'.$hapmap;	}
-		if (!empty($genome)) {		$line = $line.' - genome:'.$genome;	}
-		if (!empty($filename)) {	$line = $line.' - '.$filename;		}
-		if (!empty($message)) {		$line = $line.' - "'.$message.'"';	}
-		file_put_contents($log_file, $line . PHP_EOL, FILE_APPEND);
-	}
 ?>
