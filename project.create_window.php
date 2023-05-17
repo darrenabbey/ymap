fc9e9f71227fe4b6201ecd0a4fb8e47ac88ad683
Darren Abbey <darrenabbey.1@gmail.com>
2023-05-17 13:07:01 -0500

    Update quota exceeded comment to refer users to minimize function.

--- project.create_window.php
+++ project.create_window.php
@@ -15,7 +15,7 @@
 		// Setting boolean variable that will indicate whether the user has exceeded it's allocated space, if true the button to add new dataset will not appear
 		$exceededSpace = $quota > $currentSize ? FALSE : TRUE;
 		if ($exceededSpace) {
-			echo "<span style='color:#FF0000; font-weight: bold;'>You have exceeded your quota (".$quota."G) please clear space and then reload to add new dataset.</span><br><br>";
+			echo "<span style='color:#FF0000; font-weight: bold;'>You have exceeded your quota (".$quota."G) please clear space by deleting and/or minimizing projects and then reload to add new dataset.</span><br><br>";
 		}
 	}
 ?>
