8f005f1f09436306d84663a71b222cd3881440cd
Darren Abbey <darrenabbey.1@gmail.com>
2023-05-31 12:22:08 -0500

    Updated citations panel text.

--- panel.citations.php
+++ panel.citations.php
@@ -16,7 +16,7 @@
 	<base target="_blank">
 	Abbey DA, Funt J, Lurie-Weinberger MN, Thompson DA, Regev A, Myers CL, Berman J. 2014.<br>
 	YMAP: a pipeline for visualization of copy number variation and loss of heterozygosity in<br>
-	eukaryotic pathogens. Genome Med. 2014;6:100.
+	eukaryotic pathogens. Genome Med. 2014; 6(11):100.
 	<a href="https://pubmed.ncbi.nlm.nih.gov/25505934/">https://pubmed.ncbi.nlm.nih.gov/25505934/</a><br>
 	<p class="tab">
 	This paper introduced YMAP and described it in some detail as it was in 2014.<br>
@@ -26,18 +26,21 @@
 	</p>
 </p>
 <p>
-	Abbey D, Hickman M, Gresham D, Berman J. 2011. High-resolution SNP/CGH microarrays<br>
-	reveal the accumulation of loss of heterozygosity in commonly used Candida albicans<br>
-	strains. G3 (Bethesda) 1: 523–530.
+	Abbey D, Hickman M, Gresham D, Berman J. High-resolution SNP/CGH microarrays reveal<br>
+	the accumulation of loss of heterozygosity in commonly used Candida albicans strains<br>
+	G3 (Bethesda). 2011; 1(7):523–530.
 	<a href="https://pubmed.ncbi.nlm.nih.gov/22384363/">https://pubmed.ncbi.nlm.nih.gov/22384363/</a><br>
 	<p class="tab">
 	This paper described the construction of a hapmap for <i>C. albicans</i> strains derived from SC5314, using custom designed microarrays.<br>
 	</p>
 </p>
+</font>
 
 <font size='3'><b>Papers which have cited YMAP.</b></font><br>
-<p class="tab">
-	At one time I was collecting a list of specific papers that cited YMAP, but for ease of capturing new citations I will refer you to
+<font size="2">
+<p>
+	At one time I was collecting a list of specific papers that cited YMAP, but for ease of capturing<br>
+	new citations I will refer you to
 	<a href="https://pubmed.ncbi.nlm.nih.gov/?linkname=pubmed_pubmed_citedin&from_uid=25505934">PubMed</a> and 
 	<a href="https://scholar.google.com/scholar?cites=15360153506490453844&as_sdt=5,24&sciodt=0,24&hl=en">Google Scholar</a>
 </p>
