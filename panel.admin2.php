d9838421418123fa099771dfe47ec714cf011876
Darren Abbey <darrenabbey.1@gmail.com>
2023-06-02 01:24:41 -0500

    add lock/unlock feature to admin panel 2.

--- panel.admin2.php
+++ panel.admin2.php
@@ -204,6 +204,11 @@
 				echo "localStorage.setItem(\"projectKey\",\"".$key."\");";
 				echo "localStorage.setItem(\"projectName\",\"".$project."\");";
 			echo "'>";
+			if (file_exists("users/".$user."/projects/".$project."/locked.txt")) {
+				echo "<input type='button' value='Unlock project' onclick=\"user = '$user'; key = '$key'; $.ajax({url:'admin.unlockUserProject_server.php',type:'post',data:{key:key,user:user},success:function(answer){console.log(answer);}}); parent.update_interface(); setTimeout(()=> {location.replace('panel.admin3.php')},500);\">\n";
+			} else {
+				echo "<input type='button' value='Lock project'   onclick=\"user = '$user'; key = '$key'; $.ajax({url:'admin.lockUserProject_server.php',type:'post',data:{key:key,user:user},success:function(answer){console.log(answer);}}); parent.update_interface(); setTimeout(()=> {location.replace('panel.admin3.php')},500);\">\n";
+			}
 			echo "</form>";
 		}
 		echo "</font></span>\n\t\t\t\t";
