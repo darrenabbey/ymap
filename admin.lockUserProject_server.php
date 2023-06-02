3a85f48c8547627c67ee4bffca34f3d43a94a04c
Darren Abbey <darrenabbey.1@gmail.com>
2023-06-02 00:56:57 -0500

    properly identify project to be locked.

--- admin.lockUserProject_server.php
+++ admin.lockUserProject_server.php
@@ -37,10 +37,30 @@
 	array_multisort($projectFolders, SORT_ASC, $projectFolders);
 
 	// Trim path from each folder string.
-	foreach($projectFolders as $key=>$folder) {
-		$projectFolders[$key] = str_replace($projectDir,"",$folder);
+	foreach($projectFolders as $key=>$folder) {   $projectFolders[$key] = str_replace($projectDir,"",$folder);   }
+
+	// Split project list into ready/working/starting lists for sequential display.
+	$projectFolders_complete = array();
+	$projectFolders_working  = array();
+	$projectFolders_starting = array();
+	foreach($projectFolders as $key=>$project) {
+		if (file_exists("users/".$admin_as_user."/projects/".$project."/complete.txt")) {
+			array_push($projectFolders_complete,$project);
+		} else if (file_exists("users/".$admin_as_user."/projects/".$project."/working.txt")) {
+			array_push($projectFolders_working, $project);
+		} else if (is_dir("users/".$admin_as_user."/projects/".$project)) {
+			array_push($projectFolders_starting,$project);
+		}
 	}
-	$project_target = $projectFolders[$project_key];
+	$userProjectCount_starting = count($projectFolders_starting);
+	$userProjectCount_working  = count($projectFolders_working);
+	$userProjectCount_complete = count($projectFolders_complete);
+	// Sort complete and working projects alphabetically.
+	array_multisort($projectFolders_working,  SORT_ASC, $projectFolders_working);
+	array_multisort($projectFolders_complete, SORT_ASC, $projectFolders_complete);
+
+	// grab selected project by it's key
+	$project_target = $projectFolders_working[$project_key];
 
 	// Confirm if requested user and project exists.
 	$dir     = "users/".$project_user."/projects/".$project_target;
