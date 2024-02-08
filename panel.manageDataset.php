4bea3e3c6e101be7dc75af8e12a292041b10a47d
Darren Abbey <darrenabbey.1@gmail.com>
2024-02-07 21:08:08 -0600

    Add background color management to labels. Add informative comments. Fix bug that was interfering with user interface presentation of project status.

--- panel.manageDataset.php
+++ panel.manageDataset.php
@@ -68,8 +68,12 @@
 		?><br>
 		<b><font size='2'>Datasets Pending</font></b><br>
 		<div class='tab' style='color:#CC0000; font-size:10pt;' id='newly_installed_list' name='newly_installed_list'></div><br>
-		<div style='color:#CC0000; font-size:10pt; visibility:hidden; text-align:left;' id='pending_comment' name='pending_comment'>Reload page after any current uploads have completed to prepare pending datasets
-		for upload.<br><br>Additional datasets can be defined with the 'Install New Dataset' button while files upload.<br></div>
+
+		<div style='color:#CC0000; font-size:10pt; visibility:hidden; text-align:left;' id='pending_comment' name='pending_comment'>Reload page after any current uploads have completed to
+		prepare pending datasets for upload.<br><br>Additional datasets can be defined with the 'Install New Dataset' button while files upload.</div>
+
+		<div style='color:#CC0000; font-size:10pt; visibility:hidden; text-align:left;' id='bulk_comment' name='pending_comment'>Reload page to see status of bulk dataset processing.</div>
+
 		<div style='color:#CC0000; font-size:10pt; visibility:hidden; text-align:center;' id='name_error_comment' name='name_error_comment'>(Entered dataset name is already in use.)</div>
 		<?php
 	}
@@ -93,11 +97,14 @@
 
 		// Split project list into ready/working/starting lists for sequential display.
 		$projectFolders_starting = array();
+		$projectFolders_bulk     = array();
 		$projectFolders_working  = array();
 		$projectFolders_complete = array();
 		foreach($projectFolders as $key=>$project) {
 			if (file_exists("users/".$user."/projects/".$project."/complete.txt")) {
 				array_push($projectFolders_complete,$project);
+			} else if (file_exists("users/".$user."/projects/".$project."/bulk.txt")) {
+				array_push($projectFolders_bulk, $project);
 			} else if (file_exists("users/".$user."/projects/".$project."/working.txt")) {
 				array_push($projectFolders_working, $project);
 			} else if (is_dir("users/".$user."/projects/".$project)) {
@@ -105,15 +112,17 @@
 			}
 		}
 		$userProjectCount_starting = count($projectFolders_starting);
+		$userProjectCount_bulk     = count($projectFolders_bulk);
 		$userProjectCount_working  = count($projectFolders_working);
 		$userProjectCount_complete = count($projectFolders_complete);
 
-		// Sort complete and working projects alphabetically.
-		array_multisort($projectFolders_working,  SORT_ASC, $projectFolders_working);
+		// Sort bulk, working, and complete projects alphabetically.
+		array_multisort($projectFolders_bulk,     SORT_ASC, $projectFolders_bulk    );
+		array_multisort($projectFolders_working,  SORT_ASC, $projectFolders_working );
 		array_multisort($projectFolders_complete, SORT_ASC, $projectFolders_complete);
 		// Build new 'projectFolders' array;
 		$projectFolders   = array();
-		$projectFolders   = array_merge($projectFolders_starting, $projectFolders_working, $projectFolders_complete);
+		$projectFolders   = array_merge($projectFolders_starting, $projectFolders_bulk, $projectFolders_working, $projectFolders_complete);
 		$userProjectCount = count($projectFolders);
 		// displaying size if it's bigger then 0
 		if ($currentSize > 0) {
@@ -126,19 +135,28 @@
 
 		foreach($projectFolders_starting as $key_=>$project) {
 			if (!$exceededSpace) {
-				printprojectInfo("3", $key_, "CC0000", $user, $project);
+				printprojectInfo("3", $key_, "CC0000", "FFFFFF", $user, $project);
 			} else {
-				printprojectInfo("4", $key_, "888888", $user, $project);
+				printprojectInfo("4", $key_, "888888", "FFFFFF", $user, $project);
 			}
 			$key_starting = $key;
 		}
+		foreach($projectFolders_bulk as $key_=>$project) {
+			printprojectInfo("5", $key_ + count($projectFolders_starting), "000000", "FFCCCC", $user, $project);
+			$key_working = $key;
+		}
 		foreach($projectFolders_working as $key_=>$project) {
-			printprojectInfo("2", $key_ + count($projectFolders_starting), "BB9900", $user, $project);
+			printprojectInfo("2", $key_ + count($projectFolders_bulk) + count($projectFolders_starting), "BB9900", "FFFFFF", $user, $project);
 			$key_working = $key;
 		}
 		foreach($projectFolders_complete as $key_=>$project) {
-			printprojectInfo("1", $key_ + count($projectFolders_starting) + count($projectFolders_working), "00AA00", $user, $project);
+			printprojectInfo("1", $key_ + count($projectFolders_bulk)+ count($projectFolders_starting) + count($projectFolders_working), "00AA00", "FFFFFF", $user, $project);
 		}
+		// 1: project complete.
+		// 2: project working.
+		// 3: project starting, quota not filled.
+		// 4: project starting, quota filled.
+		// 5: project in bulk-processing-queue.
 
 ?>
 <script type='text/javascript'>
@@ -149,14 +167,21 @@
 <?php
 	}
 
-	function printProjectInfo($frameContainerIx, $key, $labelRgbColor, $user, $project) {
+	function printProjectInfo($frameContainerIx, $key, $labelRgbColor, $labelRgbBackgroundColor, $user, $project) {
+		// $frameContainerIx values:
+		//	1: project complete.
+		//	2: project working.
+		//	3: project starting, quota not filled.
+		//	4: project starting, quota filled.
+		//	5: project in bulk-processing-queue.
+
 		$projectNameFile = "users/".$user."/projects/".$project."/name.txt";
 		$projectNameString = file_get_contents($projectNameFile);
 		$projectNameString = trim($projectNameString);
 
 		$projectNameString = file_get_contents("users/".$user."/projects/".$project."/name.txt");
 		$projectNameString  = trim($projectNameString);
-		echo "<span id='p_label_".$key."' style='color:#".$labelRgbColor.";'>\n\t\t\t\t";
+		echo "<span id='p_label_".$key."' style='color:#".$labelRgbColor."; background-color:#".$labelRgbBackgroundColor.";'>\n\t\t\t\t";
 		echo "<font size='2'>".($key+1).".";
 		if (!file_exists("users/".$user."/projects/".$project."/locked.txt")) {
 			echo "<button id='project_delete_".$key."' type='button' onclick=\"parent.deleteProjectConfirmation('".$project."','".$key."');\">Delete</button>";
@@ -246,7 +271,7 @@ var systemProjectCount = "<?php echo $systemProjectCount; ?>";
 //| Uploading data |
 //'----------------'
 if (isset($_SESSION['logged_on'])) {
-	foreach($projectFolders_starting as $key_=>$project) {    // frameContainer.p3_[$key] : starting.
+	foreach($projectFolders_starting as $key_=>$project) {	// frameContainer.p3_[$key] : starting.
 		$key      = $key_;
 		$project  = $projectFolders[$key];
 		// Read in dataFormat string for project.
@@ -303,12 +328,22 @@ if (isset($_SESSION['logged_on'])) {
 			echo "p_js.dataFormat        = 'ddRADseq_single';\n";
 		}
 	}
-	foreach($projectFolders_working as $key_=>$project) {   // frameContainer.p2_[$key] : working.
+	foreach($projectFolders_bulk as $key_=>$project) {      // frameContainer.p5_[$key] : in bulk-processing queue.
 		$key      = $key_ + $userProjectCount_starting;
 		$project  = $projectFolders[$key];
-		$handle   = fopen("users/".$user."/projects/".$project."/dataFormat.txt", "r");
-		$dataFormat = fgets($handle);
-		fclose($handle);
+		echo "\n// javascript for project #".$key.", '".$project."'\n";
+		echo "var el_p            = document.getElementById('frameContainer.p5_".$key."');\n";
+		echo "el_p.innerHTML      = '<iframe id=\"p_".$key."\" name=\"p_".$key."\" class=\"upload\" style=\"height:38px; border:0px;\" ";
+		echo     "src=\"project.working.php\" marginwidth=\"0\" marginheight=\"0\" vspace=\"0\" hspace=\"0\" width=\"100%\" frameborder=\"0\"></iframe>';\n";
+		echo "var p_iframe        = document.getElementById('p_".$key."');\n";
+		echo "var p_js            = p_iframe.contentWindow;\n";
+		echo "p_js.user           = \"".$user."\";\n";
+		echo "p_js.project        = \"".$project."\";\n";
+		echo "p_js.key            = \"p_".$key."\";\n";
+	}
+	foreach($projectFolders_working as $key_=>$project) {   // frameContainer.p2_[$key] : working.
+		$key      = $key_ + $userProjectCount_starting + $userProjectCount_bulk;
+		$project  = $projectFolders[$key];
 		echo "\n// javascript for project #".$key.", '".$project."'\n";
 		echo "var el_p            = document.getElementById('frameContainer.p2_".$key."');\n";
 		echo "el_p.innerHTML      = '<iframe id=\"p_".$key."\" name=\"p_".$key."\" class=\"upload\" style=\"height:38px; border:0px;\" ";
