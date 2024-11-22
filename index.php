29948e7cb60e59c2cced639c603c58ad6c50036f
Darren Abbey <darrenabbey.1@gmail.com>
2024-11-22 02:17:35 -0600

    Fix typo that prevented new genome installation from being finalized.

--- index.php
+++ index.php
@@ -702,7 +702,7 @@ function blank_and_content_tab() {
 	<td width="5%" align="right"><div onmousedown="document.getElementById('Hidden_InstallNewGenome2').style.display = 'none';" style="display:inline-block;"><b>[X]</b></div></td>
 	</tr></table>
 	<br>
-	iframe id="Hidden_InstallNewGenome2_Frame" src="genome.create_window2.php"></iframe>
+	<iframe id="Hidden_InstallNewGenome2_Frame" src="genome.create_window2.php"></iframe>
 </DIV>
 <DIV id="Hidden_GenerateNewHapmap" style="display: none; position: absolute; border: solid black 1px; padding: 5px; text-align: justify;">
 	<table width="100%"><tr>
