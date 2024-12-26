84b0730ab9cbd455e24ef710abf14986184fb57e
Darren Abbey <darrenabbey.1@gmail.com>
2024-12-26 03:19:44 -0600

    Simplify updating chr_bin_SNP width in one step, from server-side access only.

--- scripts_genomes/genome.reinstall.sh
+++ scripts_genomes/genome.reinstall.sh
@@ -7,7 +7,15 @@ set -e;
 ## All created files will have permission 760
 umask 007;
 
+##
+## Script changes the chr_bin_SNP width used for figures using this genome.
+## After running this script, update figures for dataset of interest using YMAP:UI.
+##
+
 user="default";
 genome="Candida_parapsilosis_CDC317_s01-m03-r62_CGD";
+
+echo "70" > /var/www/html/ymap/users/$user/genomes/$genome/resolution.SNPs.txt;
+rm /var/www/html/ymap/users/$user/genomes/$genome/datafile_g_0.standard_bins.SNPs.fasta;
 
 sh genome.install_6.sh $user $genome;
