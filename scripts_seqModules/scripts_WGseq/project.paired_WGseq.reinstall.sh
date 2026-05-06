82e6b4a590a89d66bf0fdb833afd9c825840481f
Darren Abbey <darrenabbey.1@gmail.com>
2026-05-06 17:04:19 -0500

    Deleted unused troubleshooting scripts.

--- scripts_seqModules/scripts_WGseq/project.paired_WGseq.reinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-#
-# project.paired_WGseq.install_3.sh
-#
-user="darren1";
-project="test_WGseq_single";
-
-sh project.paired_WGseq.install_3.sh $user $project;
--- scripts_seqModules/scripts_WGseq/project.single_WGseq.reinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-#
-# project.single_WGseq.install_3.sh
-#
-user="darren1";
-project="test_WGseq_single";
-
-sh project.single_WGseq.install_3.sh $user $project;
