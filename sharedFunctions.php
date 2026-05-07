4588fc286188ca9f5a3a422e104adcfb45b1062b
Darren Abbey <darrenabbey.1@gmail.com>
2026-05-07 00:08:36 -0500

    Update queue_start function to look for pre-existing init entry.

--- sharedFunctions.php
+++ sharedFunctions.php
@@ -99,7 +99,6 @@ function queue_init($user,$project,$genome,$hapmap,$message) {
 	if (!empty($message)) {   $line = $line.' - '.$message;           }
 	file_put_contents($log_file, $line . PHP_EOL, FILE_APPEND);
 }
-
 function queue_start($user,$project,$genome,$hapmap,$message) {
 	// find main Ymap directory, by removing possible ymap subdirectories from path of calling script.
 	$filePath = getcwd();
@@ -111,26 +110,48 @@ function queue_start($user,$project,$genome,$hapmap,$message) {
 	$filePath = str_replace("/scripts_hapmaps","",$filePath);
 	$filePath = str_replace("/scripts_ddRADseq","",$filePath);
 
-	// define log file.
-	$log_file = $filePath."/queue/".date('Y-m-d')."_queue.log";
-
-	// check if log file exists, create if not.
-	if (!file_exists($log_file)) {
-		$myfile = fopen($log_file, "w");
-		fwrite($myfile, "");
-		fclose($myfile);
-		chmod($log_file, 0774);
+	// Find which existing queue file contains the init for this project/genome/hapmap.
+	$queue_dir   = $filePath."/queue/";
+	$queue_files = array_slice(scandir($queue_dir), 2);
+	foreach ($queue_files as $key1 => $queue_file) {
+		if (str_contains($queue_file,".log")) {
+			$queue_contents = trim(file_get_contents($queue_dir.$queue_file));
+			if ($queue_contents) {
+				// Queue contents example:
+				//      Initiate queue file: 2026-05-05 19:06:21
+				//      2026-05-05 19:06:21 - user:darrenFY - project:TJ4771_R1_clean - start - message 1.
+				//      2026-05-05 19:06:21 - user:darrenFY - project:TJ4772_R1_clean - start - message 2.
+				//      2026-05-05 19:06:21 - user:darrenFY - project:TJ4773_R1_clean - start
+				$outline = "";
+				$queue_lines = preg_split("/\R/", $queue_contents);
+				foreach($queue_lines as $key2 => $line){
+					$line_parts = explode(" - ",$line);
+					if (sizeof($line_parts) > 0) {
+						if ($line_parts[1] == "user:".$user) {
+							if ($line_parts[2] == "project:".$project) {
+								$outline = date('Y-m-d H:i:s');
+								$outline = $outline.' - user:'.$user;
+								$outline = $outline.' - project:'.$project;
+							} else if ($line_parts[2] == "genome:".$genome) {
+								$outline = date('Y-m-d H:i:s');
+								$outline = $outline.' - user:'.$user;
+								$outline = $outline.' - genome:'.$genome;
+							} else if ($line_parts[2] == "hapmap:".$hapmap) {
+								$outline = date('Y-m-d H:i:s');
+								$outline = $outline.' - user:'.$user;
+								$outline = $outline.' - hapmap:'.$hapmap;
+							}
+						}
+					}
+				}
+				if ($outline <> "") {
+					$outline = $outline.' - start';
+					if (!empty($message)) {   $outline = $outline.' - '.$message;           }
+					file_put_contents($queue_dir.$queue_file, $outline.PHP_EOL, FILE_APPEND);
+				}
+			}
+		}
 	}
-
-	// add comment to log file.
-	$line = date('Y-m-d H:i:s');
-	$line = $line.' - user:'.$user;
-	if (!empty($project)) {   $line = $line.' - project:'.$project;   }
-	if (!empty($genome)) {    $line = $line.' - genome:'.$genome;     }
-	if (!empty($hapmap)) {    $line = $line.' - hapmap:'.$hapmap;     }
-	$line = $line.' - start';
-	if (!empty($message)) {   $line = $line.' - '.$message;           }
-	file_put_contents($log_file, $line . PHP_EOL, FILE_APPEND);
 }
 function queue_end($user,$project,$genome,$hapmap,$message) {
 	// find main Ymap directory, by removing possible ymap subdirectories from path of calling script.
@@ -143,6 +164,7 @@ function queue_end($user,$project,$genome,$hapmap,$message) {
 	$filePath = str_replace("/scripts_hapmaps","",$filePath);
 	$filePath = str_replace("/scripts_ddRADseq","",$filePath);
 
+	// Find which existing queue file contains the init for this project/genome/hapmap.
 	$queue_dir   = $filePath."/queue/";
 	$queue_files = array_slice(scandir($queue_dir), 2);
 	foreach ($queue_files as $key1 => $queue_file) {
