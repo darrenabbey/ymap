ddcf3f7afbd5d5fc363911d93a5f0212fa9755c3
Darren Abbey <darrenabbey.1@gmail.com>
2026-05-13 03:55:58 -0500

    Condense YMAP commandline tools into one script.

---
+++ YMAPcl.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+set -e;
+
+lineThick="#================================================================================#";
+lineThin="#--------------------------------------------------------------------------------#";
+
+if [ -z $1 ]; then
+	echo -e $lineThick;
+	echo -e "# YMAP2 commandline";
+	echo -e $lineThin;
+	echo -e "#";
+	echo -e "# Command syntax is : 'bash YMAPcl.sh [command] (option1) (option2) (...)'";
+	echo -e "# ";
+	echo -e "#   Commands:";
+	echo -e "#        users           : List registered users.";
+	echo -e "#        user (user)     : Show user account information.";
+	echo -e "#        status (user)   : Shows data processing status.";
+	echo -e "#        projects (user) : Lists installed projects.";
+	echo -e "#        genomes (user)  : Lists installed genomes.";
+	echo -e "#        hapmaps (user)  : Lists installed hapmaps.";
+	echo -e "#        complete (user) : Lists figure images for completed projects.";
+	echo -e "#";
+	echo -e "#   Commands to be written:";
+	echo -e "#        install dataset (user)";
+	echo -e "#        install genome (user)";
+	echo -e "#        build hapmap (user)";
+	echo -e "#        minimize dataset (user)";
+	echo -e "#        delete dataset (user)";
+	echo -e "#        delete genome (user)";
+	echo -e "#        delete hapmap (user)";
+	echo -e "#        combine figures (user)";
+	echo -e "# ";
+	echo -e $lineThick;
+else
+	main_dir=$(pwd);
+	userDirectory=$main_dir"/users/";
+	echo -e $lineThick;
+	case $1 in
+	    "users")
+		echo -e "# YMAP2 commandline : List users.";
+		echo -e $lineThin;
+		echo -e "#";
+		if [ -d $userDirectory ]; then
+			echo -e "# Registered user accounts:";
+			dirs=$(find $userDirectory* -maxdepth 0 -type d);
+			if [ -z "$dirs" ]; then
+				echo -e "#\tNo registered users."
+			else
+				for dir in $dirs; do
+					echo -e "#\t"${dir##*/};
+				done;
+			fi;
+		else
+			echo -e "#\t\e[41mError: User directory not found!\e[0m";
+		fi;
+	    ;;
+	    "user")
+		echo -e "# YMAP2 commandline : User information.";
+		echo -e $lineThin;
+		echo -e "#";
+		if [ -z $2 ]; then
+			echo -e "#\tUsage: bash YMAPcl.sh user \e[31m(user)\e[0m";
+		else
+			echo -e "# user : "$2;
+			echo -e "#";
+			main_dir=$(pwd);
+			userInfoFile=$main_dir"/users/"$2"/info.txt";
+			#cat $userInfoFile;
+			while IFS= read -r line; do
+				echo -e "#\t"$line;
+			done < $userInfoFile;
+		fi;
+	    ;;
+	    "status")
+		echo -e "# YMAP2 commandline : User project status.";
+		echo -e $lineThin;
+		echo -e "#";
+		if [ -z $2 ]; then
+			echo -e "#\tUsage: bash YMAPcl.sh status \e[31m(user)\e[0m";
+		else
+			projectDirectory=$main_dir"/users/"$2"/projects/";
+			if [ -d $projectDirectory ]; then
+				echo -e "# Projects initialized or processing:";
+				cd $projectDirectory;
+				for dir in */; do
+					line=$( tail -n 1 $dir"condensed_log.txt" )
+					if [[ "$line" != "Cleaning and archiving." ]]; then
+						echo -e "#\t"$dir"\t: "$line;
+						if [ -e $dir"error.txt" ]; then
+							error=$( cat $dir"error.txt"; )
+							echo -e "#\t\t\e[41mError: $error\e[0m";
+						fi;
+					fi;
+				done;
+				echo -e "#";
+				echo -e "# Projects completed:";
+				for dir in */; do
+					line=$( tail -n 1 $dir"condensed_log.txt" )
+					if [[ "$line" == "Cleaning and archiving." ]]; then
+						echo -e "#\t"$dir;
+					fi;
+				done;
+				cd ../../../;
+			else
+				echo -e "#\t\e[41mError: User not registered!\e[0m";
+			fi;
+		fi;
+	    ;;
+	    "projects")
+		echo -e "# YMAP2 commandline : List user projects.";
+		echo -e $lineThin;
+		echo -e "#";
+		if [ -z $2 ]; then
+			echo -e "#\tUsage: bash YMAPcl.sh projects \e[31m(user)\e[0m";
+		else
+			echo -e "# user : "$2;
+			echo -e "#";
+			main_dir=$(pwd);
+			projectDirectory=$main_dir"/users/"$2"/projects/";
+			if [ -d $projectDirectory ]; then
+				echo -e "# User installed projects:";
+				dirs=$(find $projectDirectory* -type d);
+				if [ -z "$dirs" ]; then
+					echo -e "#\tNo user installed projects."
+				else
+					for dir in $dirs; do
+						echo -e "#\t"${dir##*/};
+					done;
+				fi;
+
+			else
+				echo -e "#\t\e[41mError: User not registered!\e[0m";
+			fi;
+		fi;
+	    ;;
+	    "genomes")
+		echo -e "# YMAP2 commandline : List user genomes.";
+		echo -e $lineThin;
+		echo -e "#";
+		if [ -z $2 ]; then
+			echo -e "#\tUsage: bash YMAPcl.sh genomes \e[31m(user)\e[0m";
+		else
+			echo -e "# user : "$2;
+			echo -e "#";
+			main_dir=$(pwd);
+			genomeDirectory=$main_dir"/users/"$2"/genomes/";
+			if [ -d $genomeDirectory ]; then
+				echo -e "# User installed genomes:";
+				dirs=$(find $genomeDirectory* -type d);
+				if [ -z "$dirs" ]; then
+					echo -e "#\tNo user installed genomes."
+				else
+					for dir in $dirs; do
+						echo -e "#\t"${dir##*/};
+					done;
+				fi;
+			else
+				echo -e "#\t\e[41mError: User not registered!\e[0m";
+			fi;
+		fi;
+		echo -e "#";
+		main_dir=$(pwd);
+		genomeDirectory=$main_dir"/users/default/genomes/";
+		echo -e "# System installed genomes:";
+		dirs=$(find $genomeDirectory* -type d);
+		if [ -z "$dirs" ]; then
+			echo -e "#\tNo system installed genomes."
+		else
+			for dir in $dirs; do
+				echo -e "#\t"${dir##*/};
+			done;
+		fi;
+	    ;;
+	    "hapmaps")
+		echo -e "# YMAP2 commandline : List user hapmaps.";
+		echo -e $lineThin;
+		echo -e "#";
+		if [ -z $2 ]; then
+			echo -e "#\tUsage: bash YMAPcl.sh hapmaps \e[31m(user)\e[0m";
+		else
+			echo -e "# user : "$2;
+			echo -e "#";
+			main_dir=$(pwd);
+			hapmapDirectory=$main_dir"/users/"$2"/hapmaps/";
+			if [ -d $hapmapDirectory ]; then
+				echo -e "# User installed hapmaps:";
+				dirs=$(find $hapmapDirectory* -type d);
+				if [ -z "$dirs" ]; then
+					echo -e "#\tNo user installed hapmaps."
+				else
+					for dir in $dirs; do
+						echo -e "#\t"${dir##*/};
+					done;
+				fi;
+			else
+				echo -e "#\t\e[41mError: User not registered!\e[0m";
+			fi;
+		fi;
+		echo -e "#";
+		main_dir=$(pwd);
+		hapmapDirectory=$main_dir"/users/default/hapmaps/";
+		echo -e "# System installed hapmaps:";
+		dirs=$(find $hapmapDirectory* -type d);
+		if [ -z "$dirs" ]; then
+			echo -e "#\tNo system installed hapmaps."
+		else
+			for dir in $dirs; do
+				echo -e "#\t"${dir##*/};
+			done;
+		fi;
+	    ;;
+	    "complete")
+		echo -e "# YMAP2 commandline : List user figures.";
+		echo -e $lineThin;
+		echo -e "#";
+		if [ -z $2 ]; then
+			echo -e "#\tUsage: bash YMAPcl.sh complete \e[31m(user)\e[0m";
+		else
+			echo -e "# user: "$2;
+			projectDirectory=$main_dir"/users/"$2"/projects/";
+			if [ -d $projectDirectory ]; then
+				echo -e "#";
+					echo -e "# Projects completed:";
+					cd $projectDirectory;
+					for dir in */; do
+						line=$( tail -n 1 $dir"condensed_log.txt" )
+						if [[ "$line" == "Cleaning and archiving." ]]; then
+							echo -e "#\t"$dir;
+							for file in $projectDirectory$dir*.png; do
+								filename=${file##*/};
+								if [[ $filename != *"Rsquared"* ]]; then
+									if [[ $filename != *"ChARM_test"* ]]; then
+										if [[ $filename != *"SNP-histogram"* ]]; then
+											echo -e "#\t\tusers/"$user"/projects/"$dir${filename##*/};
+										fi;
+									fi;
+								fi;
+							done;
+						fi;
+					done;
+				cd ../../../;
+			else
+				echo -e "#\t\e[41mError: User name not registered.\e[0m";
+			fi;
+			echo -e "#";
+		fi;
+	    ;;
+	esac;
+	echo -e "#";
+	echo -e $lineThick
+fi;
+
