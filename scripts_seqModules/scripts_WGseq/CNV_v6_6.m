cb1b5f3f9798b7066a0b2b40b62b84a80e970b14
Darren Abbey <darrenabbey.1@gmail.com>
2026-05-14 01:58:12 -0500

    Adjust chromosome-end-bias fitting algorithm to avoid leaving artifacts in the center of large chromosomes.

--- scripts_seqModules/scripts_WGseq/CNV_v6_6.m
+++ scripts_seqModules/scripts_WGseq/CNV_v6_6.m
@@ -443,8 +443,8 @@ if (performEndbiasCorrection)
 
 		%% DRAGON
 		% Find minimum coordinate of fit, then apply that value to every location to the right in the fit (towards the chromosome center).
-		[$minFitY, $minFitYkey] = min($fitY1);
-		$fitY1($minFitYkey:end) = $minFitY;
+		[minFitY, minFitYkey] = min(fitY1);
+		fitY1(minFitYkey:end) = minFitY;
 
 		% Correct data using normalization to LOWESS fitting
 		Y_target = 1;
