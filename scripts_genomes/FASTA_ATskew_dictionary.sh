755a51238a14ca42df470c135c977b03d2184bf3
Darren Abbey <darrenabbey.1@gmail.com>
2026-05-20 04:35:38 -0500

    fix missing varible definition.

--- scripts_genomes/FASTA_ATskew_dictionary.sh
+++ scripts_genomes/FASTA_ATskew_dictionary.sh
@@ -41,6 +41,7 @@ ATskew_file=$mainDir"users/"$userAccount"/genomes/"$genomeName"/datafile_g_0.ATs
 
 # Make temp dir.
 tempdir=$(mktemp -d);
+CALLDIR=${PWD};
 #========================
 
 	# copy input files to temp dir.
