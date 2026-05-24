d00b89dc6a3d76031102a835a977db6e7fe7fc2b
Darren Abbey <darrenabbey.1@gmail.com>
2026-05-24 13:54:40 -0500

    Moving daemon sh file to template.

--- config_template.sh
+++ config_template.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+#=============================================================
+# Adjust number of cores to your server environment.
+#	Fewer cores will lead to less memory utilization.
+#	Fewer cores will lead to longer processing times.
+#
+# After making adjustments, save this file to: "config.sh"
+#-------------------------------------------------------------
+
 # How many cores is Ymap allowed to use (relevant to 3-rd party tools):
 cores=6
 
