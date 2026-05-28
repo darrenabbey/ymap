4e9823d6fa3b6aeeb7b75e9a3888617745d99db2
Darren Abbey <darrenabbey.1@gmail.com>
2026-05-28 18:54:24 -0500

    fix admin email display issues.

--- index.php
+++ index.php
@@ -172,7 +172,7 @@
 		<img src="images/YMAP2_Logo_1.png" alt="YMAP; Yeast Mapping Analysis Pipeline"><br><br>
 		</td></tr><tr valign="bottom"><td align="middle">
 <font size='2'>
-	For support, please contact us at <a href="mailto:<?php print($ADMIN_EMAIL) ?>"><?php print($admin_email) ?></a><br/><br/>
+	For support, please contact us at <a href="mailto:<?php print($ADMIN_EMAIL); ?>"><?php print($ADMIN_EMAIL) ?></a><br/><br/>
 	<button onclick="Generate_combined_figure();">Combine figures viewed below.</button><br>
 <?php
 	// Defines the filenames for combined figures.
--- panel.bugs.php
+++ panel.bugs.php
@@ -1,7 +1,11 @@
+<?php
+        require_once 'constants.php';
+        require_once 'sharedFunctions.php';
+?>
+
 <style type="text/css">
 	html * {
 		font-family: arial !important;
 	}
 </style>
-
-To report bugs, request features, ask for assistance, or otherwise get in touch, please e-mail us at <a href="mailto:darrenabbey.ymap@gmail.com">darrenabbey.ymap@gmail.com</a>.
+To report bugs, request features, ask for assistance, or otherwise get in touch, please e-mail us at <a href="mailto:<?php print($ADMIN_EMAIL); ?>"><?php print($ADMIN_EMAIL); ?></a>.
