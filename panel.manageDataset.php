2655033e012c962a401d23c63b8ee45b75dc0f7c
Darren Abbey <darrenabbey.1@gmail.com>
2026-06-10 14:37:25 -0500

    bug hunting

--- panel.manageDataset.php
+++ panel.manageDataset.php
@@ -43,7 +43,7 @@
 					echo "<b>There are currently ".$queue_count." datasets in the queue.</b> Each takes ~".$QUEUE_TIME_ESTIMATE." minutes to complete. Data uploaded now will start processing in ~".number_format(($queue_count*$QUEUE_TIME_ESTIMATE/60),1)." hours.";
 				}
 			} else {
-		//		echo "<b>There are currently no datasets in the queue.</b> Each takes ~40 minutes to complete.";
+				echo "<b>There are currently no datasets in the queue.</b> Each takes ~40 minutes to complete.";
 			}
 		?></li>
 		<li>Filenames should only have alphanumeric characters (letters, numbers, underscores and dashes) in their names (no spaces or other special characters!).</li>
