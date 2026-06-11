eabbd3735d4f28cc40907970a9c3ff8b3e5316f8
Darren Abbey <darrenabbey.1@gmail.com>
2026-06-11 00:05:21 -0500

    fix bug, wc should have been head.

--- process_input_files.php
+++ process_input_files.php
@@ -492,9 +492,9 @@ function validate_fastq($projectPath,$name_new,$condensedLogOutput,$logOutput,$e
 		// Is this a short-read or long-read fastq file?
 
 		fwrite($condensedLogOutput, "Calculating FASTQ read length statistics.\n");
-		$null            = shell_exec("wc -n 4000 ".$projectPath.$name_new." | sed -n '2~4p' > ".$projectPath.$name_new.".temp");	// Discared FASTQ lines except for sequence.
+		$null            = shell_exec("head -n 4000 ".$projectPath.$name_new." | sed -n '2~4p' > ".$projectPath.$name_new.".temp");	// Discared FASTQ lines except for sequence, for the first 1000 reads.
 		$maxReadLength   = (int)explode(" ",trim(shell_exec("wc -L ".$projectPath.$name_new.".temp")))[0];				// Get longest sequence length.
-		unlink($projectPath.$name_new.".temp");
+		//unlink($projectPath.$name_new.".temp");
 
 		fwrite($logOutput, "\t\t| max read length = ".(string)$maxReadLength."\n");
 		if ($maxReadLength <= 500) {
