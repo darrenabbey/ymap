6cd003b9d9e7fb3645e5228a14a60c687a188082
Darren Abbey <darrenabbey.1@gmail.com>
2026-06-15 01:32:26 -0500

    fix typo

--- scripts_seqModules/scripts_WGseq/project.single_WGseq.install_3.sh
+++ scripts_seqModules/scripts_WGseq/project.single_WGseq.install_3.sh
@@ -77,7 +77,7 @@ if [[ "$MAX_FASTQ_TARGET" > "0" ]]; then
 	echo -e "#\tMAX_PROCESSED_DATA_SIZE (GB) = $MAX_PROCESSED_DATA_SIZE" >> $logName;
 
 	if [[ $(echo "$FILESIZE_GB > $MAX_PROCESSED_DATA_SIZE" | bc -l) = 1 ]]; then
-		echo -e "#\tFILESIZE_GB > MAX_PROCESSED_DATA_SIZE => FASTQ subsampling needed.\n" >> $logName;
+		echo -e "#\tFILESIZE_GB > MAX_PROCESSED_DATA_SIZE => FASTQ subsampling needed." >> $logName;
 		echo -e "#------------------------------------------------------------------------------" >> $logName;
 		# Calculate fraction of target vs original.
 		TARGET_FRACTION=$(echo "$MAX_PROCESSED_DATA_SIZE/$FILESIZE_GB" | bc -l);	# Calculate the target number of paired reads.
