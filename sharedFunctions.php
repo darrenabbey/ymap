e600b0963bb3c4cb23aef263cf9dbb4859b4fd8c
Darren Abbey <darrenabbey.1@gmail.com>
2026-06-15 01:53:06 -0500

    cleanup minimize.

--- sharedFunctions.php
+++ sharedFunctions.php
@@ -58,13 +58,13 @@ function minimizeProject($dir) {
 
 	// Move files we want to keep into temp folder.
 	foreach ($files as $file) {
-		// Move specific text files.
-		if (in_array($file, array("complete.txt","dataFormat.txt","genome.txt","index.php","name.txt","parent.txt","process_log.txt"."figVer.txt","working_done.txt"))) {
-			rename($dir.$file, $temp_dir.$file);
-		}
+	//	// Move specific text files.
+	//	if (in_array($file, array("complete.txt","dataFormat.txt","genome.txt","index.php","name.txt","parent.txt","process_log.txt"."figVer.txt","working_done.txt"))) {
+	//		rename($dir.$file, $temp_dir.$file);
+	//	}
 		$file_ext = substr(strrchr($file, '.'), 1);
-		// Move [png|eps|bed|gff3] files.
-		if (($file_ext == "png") or ($file_ext == "eps") or ($file_ext == "bed") or ($file_ext == "gff3")) {
+		// Move [png|eps|bed|gff3|txt] files.
+		if (($file_ext == "png") or ($file_ext == "eps") or ($file_ext == "bed") or ($file_ext == "gff3") or ($file_ext == "txt")) {
 			rename($dir.$file, $temp_dir.$file);
 		}
 	}
