15def15d042509b6faedceb8634f0ff8c114626c
Darren Abbey <darrenabbey.1@gmail.com>
2026-06-16 01:13:00 -0500

    update error checking.

--- queue_init.php
+++ queue_init.php
@@ -3,12 +3,19 @@
 	require_once 'constants.php';
 	require_once 'sharedFunctions.php';
 
-	$userName    = $argv[1];
-	$projectName = $argv[2];
-	$genomeName  = "";
-	$hapmapName  = "";
-	$message     = "from: admin manual intervention.";
-
-	make_salt($userName,$projectName,$genomeName,$hapmapName);
-	queue_init($userName,$projectName,$genomeName,$hapmapName,$message);
+	if ($argc  == 3) {
+		$userName    = $argv[1];
+		$projectName = $argv[2];
+		$genomeName  = "";
+		$hapmapName  = "";
+		$message     = "from: admin manual intervention.";
+		if (is_dir($base_dir."/users/".$userName) and is_dir($base_dir."/users/".$userName."/projects/".$projectName)) {
+			make_salt($userName,$projectName,$genomeName,$hapmapName);
+			queue_init($userName,$projectName,$genomeName,$hapmapName,$message);
+		} else {
+			print_r("User and/or project name not found.\nUse: php queue_init.php [userName] [projectName]\n");
+		}
+	} else {
+		print_r("Use: php queue_init.php [userName] [projectName]\n");
+	}
 ?>
