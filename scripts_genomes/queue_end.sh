c51a65c773262b85e275221ae715aafb86c49d90
Darren Abbey <darrenabbey.1@gmail.com>
2026-06-17 02:16:53 -0500

    update queue_end functions.

--- scripts_genomes/queue_end.sh
+++ scripts_genomes/queue_end.sh
@@ -9,7 +9,7 @@ main_dir="$3";
 logName="$4";
 message="$5";
 
-Directory="$main_dir/users/$user/genomes/$genome";
+Directory="$main_dir/users/$user/projects/$project";
 
 ##==============================================================================
 ## Add project end to queue log file.
@@ -23,7 +23,8 @@ echo -e "require_once 'sharedFunctions.php';" >> $outputName;
 echo -e "queue_end('$user','$project','$genome','$hapmap','$message');" >> $outputName;
 echo -e "\$salt_string = get_salt('$user','$project','$genome','$hapmap');" >> $outputName;
 echo -e "log_stuff('$user','$project','$genome','$hapmap',\$salt_string,'YMAP_daemon: $message');" >> $outputName;
+echo -e "if (\$MINIMIZE_WHEN_DONE=True) {   SYSTEM_force_minimize('$user','$project','$main_dir');   }" >> $outputName;
+echo -e "SYSTEM_cleanup('$user','$project','$main_dir');" >> $outputName;
 echo -e "?>" >> $outputName;
 php $outputName;
-rm $outputName;
 echo -e "\tConclusion of processing data: $message" >> $logName;
--- scripts_seqModules/scripts_hapmaps/queue_end.sh
+++ scripts_seqModules/scripts_hapmaps/queue_end.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 set -e
 
-user=$1;
-hapmap=$2;
-main_dir=$3;
-logName=$4;
-message=$5;
+user="$1";
+project="";
+genome="";
+hapmap="$2";
+main_dir="$3";
+logName="$4";
+message="$5";
 
-projectDirectory=$main_dir"/users/"$user"/hapmaps/"$hapmap"/";
+Directory="$main_dir/users/$user/projects/$project";
 
 ##==============================================================================
 ## Add project end to queue log file.
 ##------------------------------------------------------------------------------
 echo -e "\tEnding queue processing." >> $logName;
-outputName=$projectDirectory"finalize.php";
+outputName="$Directory/finalize.php";
 echo -e "<?php" > $outputName;
-echo -e "chdir('"$main_dir"');" >> $outputName;
+echo -e "chdir('$main_dir');" >> $outputName;
 echo -e "require_once 'constants.php';" >> $outputName;
 echo -e "require_once 'sharedFunctions.php';" >> $outputName;
-echo -e "queue_end('"$user"','','','"$hapmap"','"$message"');" >> $outputName;
+echo -e "queue_end('$user','$project','$genome','$hapmap','$message');" >> $outputName;
+echo -e "\$salt_string = get_salt('$user','$project','$genome','$hapmap');" >> $outputName;
+echo -e "log_stuff('$user','$project','$genome','$hapmap',\$salt_string,'YMAP_daemon: $message');" >> $outputName;
+echo -e "if (\$MINIMIZE_WHEN_DONE=True) {   SYSTEM_force_minimize('$user','$project','$main_dir');   }" >> $outputName;
+echo -e "SYSTEM_cleanup('$user','$project','$main_dir');" >> $outputName;
 echo -e "?>" >> $outputName;
 php $outputName;
-rm $outputName;
+echo -e "\tConclusion of processing data: $message" >> $logName;
