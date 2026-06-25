7a15f246b4b8f5f7e0d2511c2c3900743559821c
Darren Abbey <darrenabbey.1@gmail.com>
2026-06-24 22:56:36 -0500

    adjust permissions.

--- admin.copyProjectToAdmin_server.php
+++ admin.copyProjectToAdmin_server.php
@@ -97,8 +97,7 @@
 	function recurseCopy(string $sourceDirectory, string $destinationDirectory): void {
 		$directory = opendir($sourceDirectory);
 		if (is_dir($destinationDirectory) === false) {
-			mkdir($destinationDirectory);
-			chmod("$destinationDirectory/",0766);
+			mkdir($destinationDirectory,0766);
 		}
 		while (($file = readdir($directory)) !== false) {
 			if ($file === '.' || $file === '..') {  continue;   }
