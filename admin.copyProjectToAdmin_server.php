e4ac5ef2b32ffd3d8535845319ae55595196467c
Darren Abbey <darrenabbey.1@gmail.com>
2026-06-24 23:05:49 -0500

    adjust permissions

--- admin.copyProjectToAdmin_server.php
+++ admin.copyProjectToAdmin_server.php
@@ -98,7 +98,7 @@
 		$directory = opendir($sourceDirectory);
 		if (is_dir($destinationDirectory) === false) {
 			mkdir($destinationDirectory);
-			chmod($destinationDirectory, 0766);
+			shell_exec('chmod ugo+rw $destinationDirectory');
 		}
 		while (($file = readdir($directory)) !== false) {
 			if ($file === '.' || $file === '..') {  continue;   }
