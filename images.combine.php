d948c269c6362213ba5c6c93f8c9a0fef1389756
Darren Abbey <darrenabbey.1@gmail.com>
2026-06-29 11:59:18 -0500

    testing

--- images.combine.php
+++ images.combine.php
@@ -137,10 +137,10 @@
 			// genome not found?
 			$genomeName = "";
 			$genomeDir  = "";
-			session_destroy();
-			echo "<script type='text/javascript'> parent.location.reload(); </script>";
-			//echo "<script type='text/javascript'>console.log('\$genomeName = $genomeName');</script>";
-			exit;
+			//session_destroy();
+			//echo "<script type='text/javascript'> parent.location.reload(); </script>";
+			echo "<script type='text/javascript'>console.log('\$genomeName = $genomeName');</script>";
+			//exit;
 		}
 
 		// Grab display name from 1st user project.
@@ -154,10 +154,10 @@
 			// project not found?
 			$projectName = "";
 			$projectDir  = "";
-			session_destroy();
-			echo "<script type='text/javascript'> parent.location.reload(); </script>";
-			//echo "<script type='text/javascript'>console.log('\$projectDir = $projectDir');</script>";
-			exit;
+			//session_destroy();
+			//echo "<script type='text/javascript'> parent.location.reload(); </script>";
+			echo "<script type='text/javascript'>console.log('\$projectDir = $projectDir');</script>";
+			//exit;
 		}
 
 		// Grab linear image fragments from genoem.
@@ -229,10 +229,10 @@
 				// project not found?
 				$projectName = "";
 				$projectDir  = "";
-				session_destroy();
-				echo "<script type='text/javascript'>parent.location.reload();</script>";
-				//echo "<script type='text/javascript'>console.log('$fig_project');</script>";
-				exit;
+				//session_destroy();
+				//echo "<script type='text/javascript'>parent.location.reload();</script>";
+				echo "<script type='text/javascript'>console.log('$fig_project');</script>";
+				//exit;
 			}
 
 
