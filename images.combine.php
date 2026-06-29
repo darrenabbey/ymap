dfd2cbe9d9dc9ceccd04d246beb1970486dabf4e
Darren Abbey <darrenabbey.1@gmail.com>
2026-06-29 11:44:42 -0500

    testing

--- images.combine.php
+++ images.combine.php
@@ -344,10 +344,10 @@
 				imagecopy($working4, $image_bottom, 0, $height_offset, 0, 0,  $imageWidth, $imageHeight_bottom);
 				// add in project name.
 				if ($genomeName == $newGenomeName) {
-					imagestring($working1,5,10,$height_offset+20,"{ ".$projectName." }",$black);
-					imagestring($working2,5,10,$height_offset+20,$projectName,$black);
-					imagestring($working3,5,10,$height_offset+20,$projectName,$black);
-					imagestring($working4,5,10,$height_offset+20,$projectName,$black);
+					imagestring($working1,5,10,$height_offset+20,"[ ".$projectName." ]",$black);
+					imagestring($working2,5,10,$height_offset+20,"[ ".$projectName." ]",$black);
+					imagestring($working3,5,10,$height_offset+20,"[ ".$projectName." ]",$black);
+					imagestring($working4,5,10,$height_offset+20,"[ ".$projectName." ]",$black);
 				} else {
 					imagestring($working1,5,10,$height_offset+20,$projectName." : Was not analyzed with the same genome name as above.",$black);
 					imagestring($working2,5,10,$height_offset+20,$projectName." : Was not analyzed with the same genome name as above.",$black);
