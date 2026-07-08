245e2130bbe579f1c3c3598af47b52fa44ccf8c5
Darren Abbey <darrenabbey.1@gmail.com>
2026-07-07 19:23:02 -0500

    fixbug

--- sharedFunctions.php
+++ sharedFunctions.php
@@ -285,7 +285,7 @@ function queue_reinit($user,$project,$genome,$hapmap,$message) {
 		$myfile = fopen($update_file, "w");
 		fwrite($myfile, date('Y-m-d'));
 		fclose($myfile);
-		chmod($myfile, 0774);
+		chmod($update_file, 0774);
 	}
 
 	// check if queue log file exists, create if not.
