a011d4bbc87f67b88e9c1ff1865d5adc56f23299
Darren Abbey <darrenabbey.1@gmail.com>
2026-07-09 17:15:53 -0500

    remove superflous code.

--- admin.copyProjectToAdmin_server.php
+++ admin.copyProjectToAdmin_server.php
@@ -111,14 +111,3 @@
 		closedir($directory);
 	}
 ?>
-<html>
-<body>
-<script type="text/javascript">
-	if (parent.parent.document.getElementById('panel_manageDataset_iframe').contentDocument.getElementById('pending_comment').style.visibility != 'visible') {
-		parent.parent.document.getElementById('panel_manageDataset_iframe'   ).contentWindow.location.reload();
-		parent.parent.document.getElementById('panel_visualizeDataset_iframe'   ).contentWindow.location.reload();
-		parent.parent.document.getElementById('panel_admin2_iframe'   ).contentWindow.location.reload();
-	}
-</script>
-</body>
-</html>
