7d62f85c24cf305ed7f531f3d6454b32cbf2fb8d
Darren Abbey <darrenabbey.1@gmail.com>
2026-07-09 16:53:03 -0500

    update algo.

--- scripts_seqModules/scripts_WGseq/fit_Gaussian_model_disomy_2.m
+++ scripts_seqModules/scripts_WGseq/fit_Gaussian_model_disomy_2.m
@@ -74,7 +74,7 @@ function [p1_a, p1_b, p1_c, p2_a, p2_b, p2_c, p3_a, p3_b, p3_c, Rsquared] = fit_
         %p2_a = data(round(locations(3)))/max(data);     p2_b = locations(2);    p2_c = abs(Estimates(4));
         %p3_a = data(round(locations(5)))/max(data);     p3_b = locations(3);    p3_c = abs(Estimates(2));
 	p1_a = max([data(round(locations(1))) data(round(locations(1))+1)])/max(data);		p1_b = locations(1);	p1_c = abs(Estimates(1));
-	p2_a = abs(Estimates(2));								p2_b = locations(2);	p2_c = abs(Estimates(3));
+	p2_a = data(round(locations(2)))/max(data);						p2_b = locations(2);	p2_c = abs(Estimates(3));
 	p3_a = max([data(round(locations(3))) data(round(locations(3))-1)])/max(data);		p3_b = locations(3);	p3_c = abs(Estimates(1));
 
 	skew_factor1 = 1;
@@ -159,7 +159,7 @@ function sse = fiterror(params,time,data,func_type,locations,show)
 	% Force left and right curves to have same width.
 	% Force the heights to match the data at those coordinates; or adjacent, to correct for 200 bin equal to zero for whatever reason.
 	p1_a = max([data(round(locations(1))) data(round(locations(1))+1)])/max(data);		p1_b = locations(1);	p1_c = abs(params(1));
-	p2_a = data(round(locations(2)))/max(data)						p2_b = locations(2);	p2_c = abs(params(3));
+	p2_a = data(round(locations(2)))/max(data);						p2_b = locations(2);	p2_c = abs(params(3));
 	p3_a = max([data(round(locations(3))) data(round(locations(3))-1)])/max(data);		p3_b = locations(3);	p3_c = abs(params(1));
 
 	skew_factor1 = 1;
