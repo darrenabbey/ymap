7ee8be2f80488beb151618e21f699593643a5b00
Darren Abbey <darrenabbey.1@gmail.com>
2026-07-09 17:32:58 -0500

    update fit algo.

--- scripts_seqModules/scripts_WGseq/fit_Gaussian_model_trisomy_2.m
+++ scripts_seqModules/scripts_WGseq/fit_Gaussian_model_trisomy_2.m
@@ -107,12 +107,19 @@ function [p1_a,p1_b,p1_c, p2_a,p2_b,p2_c, p3_a,p3_b,p3_c, p4_a,p4_b,p4_c, Rsquar
 	%%% This may need to be improved after testing.
 	%%%------------------------------------
 	p1_fit_L = p1_a*exp(-0.5*((time1_1-p1_b)./p1_c).^2);
-	p1_fit_R = p1_a*exp(-0.5*((time1_2-p1_b)./p1_c/(skew_factor1/(100.5-abs(100.5-p1_b))) ).^2);
+	%p1_fit_R = p1_a*exp(-0.5*((time1_2-p1_b)./p1_c/(skew_factor1/(100.5-abs(100.5-p1_b))) ).^2);
+	p1_fit_R = p1_a*exp(-0.5*((time1_2-p1_b)./p1_c).^2);
+
 	p2_fit_L = p2_a*exp(-0.5*((time2_1-p2_b)./p2_c).^2);
-	p2_fit_R = p2_a*exp(-0.5*((time2_2-p2_b)./p2_c/(skew_factor2/(100.5-abs(100.5-p2_b))) ).^2);
-	p3_fit_L = p3_a*exp(-0.5*((time3_1-p3_b)./p3_c/(skew_factor3/(100.5-abs(100.5-p3_b))) ).^2);
+	%p2_fit_R = p2_a*exp(-0.5*((time2_2-p2_b)./p2_c/(skew_factor2/(100.5-abs(100.5-p2_b))) ).^2);
+	p2_fit_R = p2_a*exp(-0.5*((time2_2-p2_b)./p2_c).^2);
+
+	%p3_fit_L = p3_a*exp(-0.5*((time3_1-p3_b)./p3_c/(skew_factor3/(100.5-abs(100.5-p3_b))) ).^2);
+	p3_fit_L = p3_a*exp(-0.5*((time3_1-p3_b)./p3_c).^2);
 	p3_fit_R = p3_a*exp(-0.5*((time3_2-p3_b)./p3_c).^2);
-	p4_fit_L = p4_a*exp(-0.5*((time4_1-p4_b)./p4_c/(skew_factor4/(100.5-abs(100.5-p4_b))) ).^2);
+
+	%p4_fit_L = p4_a*exp(-0.5*((time4_1-p4_b)./p4_c/(skew_factor4/(100.5-abs(100.5-p4_b))) ).^2);
+	p4_fit_L = p4_a*exp(-0.5*((time4_1-p4_b)./p4_c).^2);
 	p4_fit_R = p4_a*exp(-0.5*((time4_2-p4_b)./p4_c).^2);
 	p1_fit = [p1_fit_L p1_fit_R];
 	p2_fit = [p2_fit_L p2_fit_R];
@@ -200,14 +207,24 @@ function sse = fiterror(params,time,data,func_type,locations,show)
 	c3_  = p3_c/2 + p3_c*skew_factor3/denoms(3)/2;	p3_c = p3_c*p3_c/c3_;
 	c4_  = p4_c/2 + p4_c*skew_factor4/denoms(4)/2;	p4_c = p4_c*p4_c/c4_;
 
+	%p1_fit_L = p1_a*exp(-0.5*((time1_1-p1_b)./p1_c).^2);
+	%p1_fit_R = p1_a*exp(-0.5*((time1_2-p1_b)./p1_c/(skew_factor1/denoms(1)) ).^2);
+	%p2_fit_L = p2_a*exp(-0.5*((time2_1-p2_b)./p2_c).^2);
+	%p2_fit_R = p2_a*exp(-0.5*((time2_2-p2_b)./p2_c/(skew_factor2/denoms(2)) ).^2);
+	%p3_fit_L = p3_a*exp(-0.5*((time3_1-p3_b)./p3_c/(skew_factor3/denoms(3)) ).^2);
+	%p3_fit_R = p3_a*exp(-0.5*((time3_2-p3_b)./p3_c).^2);
+	%p4_fit_L = p4_a*exp(-0.5*((time4_1-p4_b)./p4_c/(skew_factor4/denoms(4)) ).^2);
+	%p4_fit_R = p4_a*exp(-0.5*((time4_2-p4_b)./p4_c).^2);
+
 	p1_fit_L = p1_a*exp(-0.5*((time1_1-p1_b)./p1_c).^2);
-	p1_fit_R = p1_a*exp(-0.5*((time1_2-p1_b)./p1_c/(skew_factor1/denoms(1)) ).^2);
+	p1_fit_R = p1_a*exp(-0.5*((time1_2-p1_b)./p1_c).^2);
 	p2_fit_L = p2_a*exp(-0.5*((time2_1-p2_b)./p2_c).^2);
-	p2_fit_R = p2_a*exp(-0.5*((time2_2-p2_b)./p2_c/(skew_factor2/denoms(2)) ).^2);
-	p3_fit_L = p3_a*exp(-0.5*((time3_1-p3_b)./p3_c/(skew_factor3/denoms(3)) ).^2);
+	p2_fit_R = p2_a*exp(-0.5*((time2_2-p2_b)./p2_c).^2);
+	p3_fit_L = p3_a*exp(-0.5*((time3_1-p3_b)./p3_c).^2);
 	p3_fit_R = p3_a*exp(-0.5*((time3_2-p3_b)./p3_c).^2);
-	p4_fit_L = p4_a*exp(-0.5*((time4_1-p4_b)./p4_c/(skew_factor4/denoms(4)) ).^2);
+	p4_fit_L = p4_a*exp(-0.5*((time4_1-p4_b)./p4_c).^2);
 	p4_fit_R = p4_a*exp(-0.5*((time4_2-p4_b)./p4_c).^2);
+
 	p1_fit = [p1_fit_L p1_fit_R];
 	p2_fit = [p2_fit_L p2_fit_R];
 	p3_fit = [p3_fit_L p3_fit_R];
