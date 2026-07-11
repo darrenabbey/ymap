5030a22625ccb2b09c1c2858ff9ca04be1b08fb7
Darren Abbey <darrenabbey.1@gmail.com>
2026-07-11 14:26:56 -0500

    dd

--- project.update_window.php
+++ project.update_window.php
@@ -395,7 +395,7 @@
 						</select>
 					</div>
 				</td><td valign="top">
-					<div id="hiddenFormSection6" style="display:<?php if ($hapmap == "") { echo "inline"; } else { echo "none"; }?>"></div>
+					<div id="hiddenFormSection6" <?php if ($hapmap == "") { echo "style='display:inline'"; } else { echo "style='display:none'"; }?>></div>
 				</td></tr>
 				<tr bgcolor="#CCFFCC"><td>
 					<div id="hiddenFormSection7" style="display:inline">
