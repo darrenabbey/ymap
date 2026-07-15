2279d58c303f607496e9895a6e003e6b7b6f3106
Darren Abbey <darrenabbey.1@gmail.com>
2026-07-14 20:19:02 -0500

    logging stuff.

--- scripts_seqModules/scripts_WGseq/project.WGseq.update_2.sh
+++ scripts_seqModules/scripts_WGseq/project.WGseq.update_2.sh
@@ -137,7 +137,9 @@ elif [[ -d "$main_dir/users/default/projects/$projectParent" ]]; then
 	projectParentDirectory="$main_dir/users/default/projects/$projectParent";
 	projectParentUser="default";
 fi
+echo =e "\tmain_dir               = $main_dir" >> $logName;
 echo -e "\tprojectParentDirectory = $projectParentDirectory" >> $logName;
+echo -e "\tprojectParentUser      = $projectParentUser" >> $logName;
 
 echo -e "#============================================================================== 2" >> $logName;
 
