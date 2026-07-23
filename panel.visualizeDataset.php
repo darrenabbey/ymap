571f925bbce42dda24aaa3598fc77a410c93b599
Darren Abbey <darrenabbey.1@gmail.com>
2026-07-22 23:00:07 -0500

    UI update

--- panel.visualizeDataset.php
+++ panel.visualizeDataset.php
@@ -111,14 +111,14 @@
 		//==========================================
 		// Add other projects being worked on to user interface.
 		foreach($projectFolders_working as $key_=>$project) {
-	//		if (!str_contains($project,"/")) {
+			if (!str_contains($project,"/")) {
 				$key_real = array_search($project,$projectFolders);
 				addOtherProjectsBeingWorkedOnToUserInterface($key_real,$user,$project,$prefix,$key_display);
-				if (!file_exists("users/".$user."/projects/".$project."/bulk.txt")) {
+			//	if (!file_exists("users/".$user."/projects/".$project."/bulk.txt")) {
 					$displayed_entries[] = $project;
 					$key_display += 1;
-				}
-	//		}
+			//	}
+			}
 		}
 
 		//==========================================
@@ -158,17 +158,17 @@
 				}
 			}
 
-		//	//==========================================
-		//	// Add other projects being worked on to user interface.
-		//	foreach($projectFolders_working as $key_=>$project) {
-		//		if (str_starts_with($project, $subdir . "/")) {
-		//			$key_real = array_search($project,$projectFolders);
-		//			addOtherProjectsBeingWorkedOnToUserInterface($key_real,$user,$project,$prefix,$key_display);
-		//			if (!file_exists("users/".$user."/projects/".$project."/bulk.txt")) {
-		//				$key_display += 1;
-		//			}
-		//		}
-		//	}
+			//==========================================
+			// Add other projects being worked on to user interface.
+			foreach($projectFolders_working as $key_=>$project) {
+				if (str_starts_with($project, $subdir . "/")) {
+					$key_real = array_search($project,$projectFolders);
+					addOtherProjectsBeingWorkedOnToUserInterface($key_real,$user,$project,$prefix,$key_display);
+				//	if (!file_exists("users/".$user."/projects/".$project."/bulk.txt")) {
+						$key_display += 1;
+				//	}
+				}
+			}
 
 			//==========================================
 			// Add completed projects to user interface.
