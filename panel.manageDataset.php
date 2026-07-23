9cc9644de4c1cfa8ba34009f4b71bcec6343e67b
Darren Abbey <darrenabbey.1@gmail.com>
2026-07-23 02:09:26 -0500

    UI update

--- panel.manageDataset.php
+++ panel.manageDataset.php
@@ -239,7 +239,7 @@
 		foreach($projectFolders_working as $key_=>$project) {
 			if (!str_contains($project,"/")) {
 				$key_real = array_search($project,$projectFolders);
-				printProjectInfo("2", $key_real, "000000", "FFFFCC", $user, $project,$key_offset,$prefix);
+				printProjectInfo("5", $key_real, "000000", "FFFFCC", $user, $project,$key_offset,$prefix);
 				$key_offset += 1;
 			}
 		}
